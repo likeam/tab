6ca001d2cabaf5080142d7f1251032e6503e88b3
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-29 19:15:58 +0500

    Building Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,39 @@
+import { useState } from "react"
+import List from "./List";
+import Alert from "./Alert";
+
 const App = () => {
+
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] =useState(null);
+  const [alert, setAlert] =useState({show:false, msg:'', type:''});
+  console.log(name)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  } 
+
   return (
-    <div>App</div>
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input type="text" className="grocery" placeholder="e.g. Eggs"
+          value={name} onChange={(e) => setName(e.target.value)} />
+          <button className="submit-btn" type="submit">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <div className="clear-btn" >clear items</div>
+      </div>
+    </section>
   )
 }
 export default App
